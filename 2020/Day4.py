45c7e8c5fdeb8d2e1fe1d8768bdbdb8003cb50de
CptCookie <s.hartstock@protonmail.com>
2020-12-04 16:01:24 +0100

    move test into seperate functions

--- 2020/Day4.py
+++ 2020/Day4.py
@@ -43,7 +43,8 @@ def validate_field(field, value):
     match = re.compile(VALIDATION[field]).match(value)
     return match is not None and match.group() == value
 
-if __name__ == "__main__":
+
+def test_byr_validation():
     assert(validate_field('byr', '1920'))
     assert(validate_field('byr', '2002'))
     assert(validate_field('byr', '2000'))
@@ -51,26 +52,26 @@ if __name__ == "__main__":
     assert(not validate_field('byr', '2003'))
     assert(not validate_field('byr', '1919'))
 
+def test_iyr_validation():   
     assert(validate_field('iyr', '2010'))
     assert(validate_field('iyr', '2020'))
     assert(not validate_field('iyr', '2000'))
     assert(not validate_field('iyr', '2025'))
 
-    assert(validate_field('hgt', '60in'))
-    assert(validate_field('hgt', '190cm'))
-    assert(not validate_field('hgt', '190in'))
-    assert(not validate_field('hgt', '190'))
+def test_ecl_validation():
+    assert(validate_field('ecl', 'brn'))
+    assert(not validate_field('ecl', 'wat'))
 
+def test_pid_validation():
+    assert(validate_field('pid', '000000001'))
+    assert(not validate_field('pid', '0123456789'))
+
+def test_hcl_validation():
     assert(validate_field('hcl', '#123abc'))
     assert(not validate_field('hcl', '#123abz'))
     assert(not validate_field('hcl', '123abc'))
 
-    assert(validate_field('ecl', 'brn'))
-    assert(not validate_field('ecl', 'wat'))
-
-    assert(validate_field('pid', '000000001'))
-    assert(not validate_field('pid', '0123456789'))
-
+if __name__ == "__main__":
     passports = parse_passports(get_aoc_input())
     print(f'solution 1: {sum([check_passport(n) for n in passports])}')
     print(f'solution 2: {sum([check_passport(n, validation=True) for n in passports])}')

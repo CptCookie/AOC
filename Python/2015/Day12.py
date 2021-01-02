7e91487aad48798011d6d6e077cd526aac5f19e2
CptCookie <s.hartstock@protonmail.com>
2021-01-02 22:57:20 +0100

    solution 2015 Day12 2*

---
+++ Python/2015/Day12.py
@@ -0,0 +1,35 @@
+import re
+import json
+
+def find_numbers(search):
+    regex = re.compile(r"-?\d+")
+    return [int(n) for n in regex.findall(search)]
+
+def find_without_red(search):
+    red = re.compile(r'{.+"red".+}')
+    for n in red.findall(search):
+        search = search.replace(n, "")
+    return find_numbers(search)
+
+def sum_red(obj):
+    if type(obj) == dict:
+        if "red" in obj.values():
+            return 0
+        return(sum([sum_red(n) for n in obj.values()]))
+    if type(obj) == list:
+        return sum([sum_red(n) for n in obj])
+    if type(obj) == int:
+        return obj
+    return 0
+
+def test_red():
+    assert find_without_red('[1,{"c":"red","b":2},3]') == 4
+
+def solve(puzzle_input):
+    puzzle_input = puzzle_input
+    print('testing ', end='')
+
+    print('done')
+    print('solving')
+    print(f'solution 1: {sum(find_numbers(puzzle_input))}')
+    print(f'solution 2: {sum_red(json.loads(puzzle_input))}')

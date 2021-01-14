be8a616ef0a1fda8a67d99ba4f2ffb4f55cecb43
CptCookie <s.hartstock@protonmail.com>
2021-01-14 17:55:58 +0100

    refactor main.py

--- Python/main.py
+++ Python/main.py
@@ -9,22 +9,32 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--year", '-y', help="year", type=int)
 parser.add_argument("--day", '-d', help="day", type=int)
 
-def get_input_http(year, day):
+def get_http_input(year, day):
     r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": token})
     if r.status_code == 200:
         return r.content.decode()
     else:
         raise requests.exceptions.ConnectionError('Token might be too old.')
 
+def read_cached_input(year, day):
+    with open(f'./python/cache/{year}-{day}.txt', 'r') as f:
+        return f.read()
+
+def write_cached_input(year, day, data):
+    with open(f'./python/cache/{year}-{day}.txt', 'w') as f:
+            f.write(data)
+
+def is_cached_input(year, day):
+    return f'{year}-{day}.txt' in os.listdir('./python/cache')
+
 def get_input(year, day):
-    if f'{year}-{day}.txt' in os.listdir('./python/cache'):
-        with open(f'./python/cache/{year}-{day}.txt', 'r') as f:
-            return f.read()
+    if is_cached_input(year, day):
+        return read_cached_input(year, day)
     else:
-        with open(f'./python/cache/{year}-{day}.txt', 'w') as f:
-            aoc_input = get_input_http(year, day)
-            f.write(aoc_input)
+        aoc_input = get_http_input(year, day)
+        write_cached_input(year, day, aoc_input)
         return aoc_input
+        
 
 if __name__ == "__main__":
     args = parser.parse_args()

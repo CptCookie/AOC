37f726bcfbafa1413ea1c9807f4d230f30ea46b2
CptCookie <s.hartstock@protonmail.com>
2021-02-28 17:52:28 +0100

    add more type annotation

--- Python/2015/Day2/solution.py
+++ Python/2015/Day2/solution.py
@@ -1,5 +1,5 @@
 import math
-from typing import List
+from typing import List, Callable
 from collections import namedtuple
 
 
@@ -13,13 +13,13 @@ class Box:
         areas = [self.x * self.y, self.y * self.z, self.x * self.z]
         return min(areas) + sum(areas) * 2
 
-    def ribbon_needed(self):
+    def ribbon_needed(self) -> int:
         ribbon_sides = sorted([self.x, self.y, self.z])[:-1]
         bow = self.x * self.y * self.z
         return sum(ribbon_sides) * 2 + bow
 
 
-def parse_puzzel_string(puzzle_string) -> List[Box]:
+def parse_puzzel_string(puzzle_string: str) -> List[Box]:
     box_dimensions = []
     for box in puzzle_string.split("\n"):
         if box != "":
@@ -28,16 +28,16 @@ def parse_puzzel_string(puzzle_string) -> List[Box]:
     return box_dimensions
 
 
-def total_material_needed(all_boxes, material_function):
+def total_material_needed(all_boxes: List[Box], material_function: Callable) -> int:
     material = [material_function(box) for box in all_boxes]
     return sum(material)
 
 
-def solution_1(puzzle_string):
+def solution_1(puzzle_string: str) -> int:
     all_boxes = parse_puzzel_string(puzzle_string)
     return total_material_needed(all_boxes, Box.wrapping_needed)
 
 
-def solution_2(puzzle_string):
+def solution_2(puzzle_string: str) -> int:
     all_boxes = parse_puzzel_string(puzzle_string)
     return total_material_needed(all_boxes, Box.ribbon_needed)

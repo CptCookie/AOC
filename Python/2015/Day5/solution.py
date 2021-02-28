91907ce08bea5971c5dfc457606dec4e8e48d9f7
CptCookie <s.hartstock@protonmail.com>
2021-02-28 19:32:19 +0100

    clean the code

--- Python/2015/Day5/solution.py
+++ Python/2015/Day5/solution.py
@@ -4,14 +4,18 @@ doubles = [n + n for n in alphabet]
 disalow = ["ab", "cd", "pq", "xy"]
 
 
-def is_naughty_old(text: str):
+def is_valid_string_v1(text: str):
     if sum([text.count(n) for n in vowls]) < 3:
-        return True
+        return False
     if not any([n in text for n in doubles]):
-        return True
+        return False
     if any([n in text for n in disalow]):
-        return True
-    return False
+        return False
+    return True
+
+
+def is_valid_string_v2(text):
+    return has_double(text) and has_two_apart(text)
 
 
 def has_double(text):
@@ -23,15 +27,11 @@ def has_two_apart(text):
     return any([text[n] == elem for n, elem in enumerate(text[2:])])
 
 
-def is_naughty_new(text):
-    return not has_double(text) or not has_two_apart(text)
-
-
 def solution_1(puzzle_input):
-    puzzle_input = [n for n in puzzle_input.split("\n") if n != ""]
-    return len([n for n in puzzle_input if not is_naughty_old(n)])
+    all_texts = [n for n in puzzle_input.split("\n") if n != ""]
+    return len([text for text in all_texts if is_valid_string_v1(text)])
 
 
 def solution_2(puzzle_input):
-    puzzle_input = [n for n in puzzle_input.split("\n") if n != ""]
-    return len([n for n in puzzle_input if not is_naughty_new(n)])
+    all_texts = [n for n in puzzle_input.split("\n") if n != ""]
+    return len([text for text in all_texts if not is_valid_string_v2(text)])
--- Python/2015/Day5/test_solution.py
+++ Python/2015/Day5/test_solution.py
@@ -1,4 +1,4 @@
-from .solution import has_two_apart, has_double, is_naughty_new, is_naughty_old
+from .solution import has_two_apart, has_double, is_valid_string_v1, is_valid_string_v2
 
 
 def test_has_double():
@@ -13,15 +13,15 @@ def test_two_apart():
     assert has_two_apart("uurcxstgmygtbstg") == False
 
 
-def test_naughty_old():
-    assert is_naughty_old("ugknbfddgicrmopn") == False
-    assert is_naughty_old("jchzalrnumimnmhp") == True
-    assert is_naughty_old("haegwjzuvuyypxyu") == True
-    assert is_naughty_old("dvszwmarrgswjxmb") == True
+def test_is_valid_string_v1():
+    assert is_valid_string_v1("ugknbfddgicrmopn") == True
+    assert is_valid_string_v1("jchzalrnumimnmhp") == False
+    assert is_valid_string_v1("haegwjzuvuyypxyu") == False
+    assert is_valid_string_v1("dvszwmarrgswjxmb") == False
 
 
-def test_naughty_new():
-    assert is_naughty_new("qjhvhtzxzqqjkmpb") == False
-    assert is_naughty_new("xxyxx") == False
-    assert is_naughty_new("uurcxstgmygtbstg") == True
-    assert is_naughty_new("ieodomkazucvgmuy") == True
+def test_is_valid_string_v2():
+    assert is_valid_string_v2("qjhvhtzxzqqjkmpb") == True
+    assert is_valid_string_v2("xxyxx") == True
+    assert is_valid_string_v2("uurcxstgmygtbstg") == False
+    assert is_valid_string_v2("ieodomkazucvgmuy") == False

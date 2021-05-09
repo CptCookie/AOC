98366fd06191c7356e4d8548ee2e94ffe19eb384
CptCookie <s.hartstock@protonmail.com>
2021-05-09 21:34:03 +0200

    refactor a bit

--- Rust/2018/day_2/src/main.rs
+++ Rust/2018/day_2/src/main.rs
@@ -1,24 +1,20 @@
 use std::collections::HashMap;
-
-const TOKEN:&str = "53616c7465645f5f381ca9f990b331963bc4c559f548054e919235f4337b05da8d79afdd8cd16584b77c0ee4771218fc";
-
+use std::fs;
 
 fn main() {
-    let input = read_input("https://adventofcode.com/2018/day/2/input").unwrap();
-    println!("Solution 1: {:?}", solution_1(&input));
-    println!("Solution 2: {}", solution_2(&input));
-    
-
+    let puzzle_input: String = fs::read_to_string("2018-2.txt").unwrap();
+    let ids = parse_input(puzzle_input);
+    println!("Solution 1: {:?}", solution_1(&ids));
+    println!("Solution 2: {}", solution_2(&ids));
 }
 
 fn solution_2(input: &Vec<String>) -> String {
-
-    for (n,line1) in input.iter().enumerate() {
+    for (n, line1) in input.iter().enumerate() {
         for line2 in input[n..].iter() {
             let comp = compare_lines(line1, line2);
             match comp {
                 Some(n) => return n,
-                None => ()
+                None => (),
             }
         }
     }
@@ -30,72 +26,61 @@ fn compare_lines(line1: &String, line2: &String) -> Option<String> {
     let line2: Vec<char> = line2.chars().collect();
     let mut unique: Option<usize> = None;
 
-    for (n , line1_char) in line1.chars().enumerate(){
+    for (n, line1_char) in line1.chars().enumerate() {
         if line2[n] != line1_char {
             match unique {
                 None => unique = Some(n),
-                Some (n) => return None
+                Some(_n) => return None,
             }
         }
     }
 
     if unique != None {
-        let mut newString = line2.clone();
-        newString.remove(unique.unwrap());
-        return Some(newString.into_iter().collect())
+        let mut new_string = line2.clone();
+        new_string.remove(unique.unwrap());
+        return Some(new_string.into_iter().collect());
     } else {
-        return None
+        return None;
     }
 }
 
-fn solution_1(input: &Vec<String>) -> i32{
+fn solution_1(input: &Vec<String>) -> i32 {
     let mut maps: Vec<HashMap<char, i32>> = Vec::new();
     let mut twos: i32 = 0;
     let mut threes: i32 = 0;
 
     for line in input {
-        maps.push(get_hashMap(line))
+        maps.push(get_map(line))
     }
-    
+
     for map in &maps {
-        if containes_n_chars(2, map) {
+        if containes_chars_ntimes(2, map) {
             twos += 1;
         }
 
-        if containes_n_chars(3, map) {
+        if containes_chars_ntimes(3, map) {
             threes += 1;
         }
     }
-     twos * threes
+    twos * threes
 }
 
-fn containes_n_chars(n_appear: i32, map: &HashMap<char, i32>) -> bool {
-    /// Counts how many keys appear n-times in the HashMap
-    let mut counter: i32 = 0;
+fn containes_chars_ntimes(n_appear: i32, map: &HashMap<char, i32>) -> bool {
     let content: Vec<&i32> = map.values().collect();
     content.contains(&&n_appear)
 }
 
-fn get_hashMap(line: &String) -> HashMap<char, i32>{
-    let mut hash:HashMap<char, i32> = HashMap::new();
-        for c in line.chars(){
-            let counter = hash.entry(c).or_insert(0);
-            *counter += 1;
-        }
+fn get_map(line: &String) -> HashMap<char, i32> {
+    let mut hash: HashMap<char, i32> = HashMap::new();
+    for c in line.chars() {
+        let counter = hash.entry(c).or_insert(0);
+        *counter += 1;
+    }
     hash
 }
 
-
-fn read_input(url:&str)-> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
-    let resp = client.get(url)
-                     .header(reqwest::header::COOKIE, format!("session={}", TOKEN))
-                     .send()?;
-    
-    let mut split:Vec<String> = resp.text()?
-                             .split("\n")
-                             .map(|n| String::from(n))
-                             .collect();
+fn parse_input(puzzle_input: String) -> Vec<String> {
+    let mut split: Vec<String> = puzzle_input.split("\n").map(|n| String::from(n)).collect();
     split.pop();
-    return Ok(split);
+    split
 }

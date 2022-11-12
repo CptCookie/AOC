d41e17c80fb5e94927b3c660464b672647b1c9ce
Sebastian Hartstock <sebastian.hartstock@fintiba.com>
2022-11-12 11:08:59 +0100

    made 2017.2 solution mypy compliant

--- Python/2017/Day2/solution.py
+++ Python/2017/Day2/solution.py
@@ -9,24 +9,26 @@ def parse_data(puzzle_input: str) -> List[List[int]]:
     return lines
 
 
-def get_divisables(it: Iterable[int]) -> Tuple[int, int]:
+def get_divisables(it: List[int]) -> Tuple[int, int] | Tuple[None, None]:
     for index, a in enumerate(it):
         for b in it[index + 1 :]:
             if a % b == 0:
                 return a, b
             if b % a == 0:
                 return b, a
+    return None, None
 
 
-def solution_1(puzzle_input: str):
+def solution_1(puzzle_input: str) -> int:
     data = parse_data(puzzle_input)
     return sum([max(line) - min(line) for line in data])
 
 
-def solution_2(puzzle_input: str):
+def solution_2(puzzle_input: str) -> int:
     data = parse_data(puzzle_input)
-    s = 0
+    s = .0
     for line in data:
         divident, divisor = get_divisables(line)
-        s += divident / divisor
-    return s
+        if divisor is not None and divident is not None:
+            s += divident / divisor
+    return int(s)

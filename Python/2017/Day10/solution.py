13c8ef28731490f3ad6dca2e7bc9b7a0d2a9d101
Sebastian Hartstock <sebastian.hartstock@fintiba.com>
2022-11-22 16:44:19 +0100

    2017 day 10 step 1 and a bit of step 2

--- Python/2017/Day10/solution.py
+++ Python/2017/Day10/solution.py
@@ -1,8 +1,16 @@
-def parse_data(puzzle_input: str) -> list[str]:
-    return [int(n) for n in puzzle_input.split("\t") if n != ""]
+from functools import reduce
+from operator import xor
 
 
-def knot_hash(lst: list[int], pos: int, lenght: int) -> int:
+def parse_data_int(puzzle_input: str) -> list[int]:
+    return [int(n) for n in puzzle_input.split(",") if n != ""]
+
+def parse_data_byte(puzzle_input: str) -> list[int]:
+    return [ord(n) for n in puzzle_input]
+
+
+
+def knot_lst(lst: list[int], pos: int, lenght: int) -> list[int]:
     if lenght == 1:
         return lst
 
@@ -18,13 +26,34 @@ def knot_hash(lst: list[int], pos: int, lenght: int) -> int:
         )
     else:
         i_sub_lst = lst[pos : pos + lenght][::-1]
-        print(i_sub_lst)
-        return lst[:pos] + i_sub_lst + lst[pos * lenght :]
+        return lst[:pos] + i_sub_lst + lst[pos + lenght:]
+
+def sparse_to_dense_hash(lst: list[int])-> list[int]:
+    dense = []
+    for n in range(1, 17):
+        dense.append(reduce(xor, lst[n-1::16]))
+    return dense
+
+
+def knot_hash(end: int, knots: list[int], rounds=1) -> list[int]:
+    lst = [n for n , _ in enumerate(range(0,end+1))]
+    idx = 0
+    skip_size = 0
+
+    for k in knots:
+        print(lst, idx, k, skip_size)
+        lst = knot_lst(lst, idx, k)
+        idx = (idx + k + skip_size) % len(lst)
+        skip_size += 1
+
+    return lst
+
 
 
 def solution_1(puzzle_input: str):
-    data = parse_data(puzzle_input)
-    return None
+    data = parse_data_int(puzzle_input)
+    hashed = knot_hash(255, data)
+    return hashed[0] * hashed[1]
 
 
 def solution_2(puzzle_input: str):
--- Python/2017/Day10/test_solution.py
+++ Python/2017/Day10/test_solution.py
@@ -1,13 +1,17 @@
-from .solution import knot_hash
+from .solution import knot_hash, knot_lst
 
 
-def test_knot_hash_overflow():
-    assert knot_hash([2, 1, 0, 3, 4], 3, 4) == [4, 3, 0, 1, 2]
-    assert knot_hash([4, 3, 0, 1, 2], 1, 1) == [4, 3, 0, 1, 2]
-    assert knot_hash([4, 3, 0, 1, 2], 1, 5) == [3, 4, 2, 1, 0]
+def test_knot_lst_overflow():
+    assert knot_lst([2, 1, 0, 3, 4], 3, 4) == [4, 3, 0, 1, 2]
+    assert knot_lst([4, 3, 0, 1, 2], 1, 1) == [4, 3, 0, 1, 2]
+    assert knot_lst([4, 3, 0, 1, 2], 1, 5) == [3, 4, 2, 1, 0]
+
+
+def test_knot_lst():
+    assert knot_lst([2, 1, 0, 3, 4], 0, 4) == [3, 0, 1, 2, 4]
+    assert knot_lst([4, 3, 0, 1, 2], 3, 2) == [4, 3, 0, 2, 1]
+    assert knot_lst([4, 3, 0, 1, 2], 0, 5) == [2, 1, 0, 3, 4]
 
 
 def test_knot_hash():
-    assert knot_hash([2, 1, 0, 3, 4], 0, 4) == [3, 0, 1, 2, 4]
-    assert knot_hash([4, 3, 0, 1, 2], 3, 2) == [4, 3, 0, 2, 1]
-    assert knot_hash([4, 3, 0, 1, 2], 0, 5) == [2, 1, 0, 3, 4]
+    assert knot_hash(4, [3,4,1,5]) == [3,4,2,1,0]

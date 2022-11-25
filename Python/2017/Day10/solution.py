db4529a904016eeea2e0948a66e6a8b9b66ff0cd
S. Hartstock <github@hartstock.me>
2022-11-25 08:48:30 +0100

    Solution 2017 Day 10

--- Python/2017/Day10/solution.py
+++ Python/2017/Day10/solution.py
@@ -5,49 +5,48 @@ from operator import xor
 def parse_data_int(puzzle_input: str) -> list[int]:
     return [int(n) for n in puzzle_input.split(",") if n != ""]
 
-def parse_data_byte(puzzle_input: str) -> list[int]:
-    return [ord(n) for n in puzzle_input]
 
+def parse_data_ascii(puzzle_input: str) -> list[int]:
+    return [ord(n) for n in puzzle_input.strip()] + [
+        17,
+        31,
+        73,
+        47,
+        23,
+    ]
 
 
 def knot_lst(lst: list[int], pos: int, lenght: int) -> list[int]:
-    if lenght == 1:
-        return lst
+    idxs = [(pos + l) % len(lst) for l in range(lenght)]
+    vals = reversed([lst[i] for i in idxs])
 
-    if pos + lenght > len(lst):
-
-        start_len = (pos + lenght) % len(lst)
-        sub_lst = lst[pos:] + lst[: (pos + lenght) % len(lst)]
-        i_sub_lst = sub_lst[::-1]
-        return (
-            i_sub_lst[-start_len:]
-            + lst[start_len:pos]
-            + i_sub_lst[: lenght - start_len]
-        )
-    else:
-        i_sub_lst = lst[pos : pos + lenght][::-1]
-        return lst[:pos] + i_sub_lst + lst[pos + lenght:]
-
-def sparse_to_dense_hash(lst: list[int])-> list[int]:
-    dense = []
-    for n in range(1, 17):
-        dense.append(reduce(xor, lst[n-1::16]))
-    return dense
+    for i, v in zip(idxs, vals):
+        lst[i] = v
+    return lst
 
 
 def knot_hash(end: int, knots: list[int], rounds=1) -> list[int]:
-    lst = [n for n , _ in enumerate(range(0,end+1))]
+    lst = [n for n in range(0, end + 1)]
     idx = 0
     skip_size = 0
-
-    for k in knots:
-        print(lst, idx, k, skip_size)
-        lst = knot_lst(lst, idx, k)
-        idx = (idx + k + skip_size) % len(lst)
-        skip_size += 1
+    for _ in range(rounds):
+        for k in knots:
+            lst = knot_lst(lst, idx, k)
+            idx += k + skip_size
+            skip_size += 1
 
     return lst
 
+
+def densening_hash(lst: list[int]) -> list[int]:
+    dense = []
+    for n in range(int(len(lst) / 16)):
+        dense.append(reduce(xor, lst[n * 16 : (n + 1) * 16]))
+    return dense
+
+
+def convert_to_hex(lst: int) -> str:
+    return "".join([f"{d:02x}" for d in lst])
 
 
 def solution_1(puzzle_input: str):
@@ -57,5 +56,7 @@ def solution_1(puzzle_input: str):
 
 
 def solution_2(puzzle_input: str):
-    data = parse_data(puzzle_input)
-    return None
+    data = parse_data_ascii(puzzle_input)
+    hashed = knot_hash(255, data, rounds=64)
+    hashed = densening_hash(hashed)
+    return "".join([f"{d:02x}" for d in hashed])
--- Python/2017/Day10/test_solution.py
+++ Python/2017/Day10/test_solution.py
@@ -1,4 +1,11 @@
-from .solution import knot_hash, knot_lst
+from .solution import (
+    knot_hash,
+    knot_lst,
+    parse_data_ascii,
+    densening_hash,
+    solution_2,
+    convert_to_hex,
+)
 
 
 def test_knot_lst_overflow():
@@ -14,4 +21,54 @@ def test_knot_lst():
 
 
 def test_knot_hash():
-    assert knot_hash(4, [3,4,1,5]) == [3,4,2,1,0]
+    assert knot_hash(4, [3, 4, 1, 5]) == [3, 4, 2, 1, 0]
+
+
+def test_densening_hash():
+    assert densening_hash([n for n in range(32)]) == [
+        0 ^ 1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 ^ 9 ^ 10 ^ 11 ^ 12 ^ 13 ^ 14 ^ 15,
+        16 ^ 17 ^ 18 ^ 19 ^ 20 ^ 21 ^ 22 ^ 23 ^ 24 ^ 25 ^ 26 ^ 27 ^ 28 ^ 29 ^ 30 ^ 31,
+    ]
+
+
+def test_parse_data_bit():
+    assert (parse_data_ascii("1,2,3")) == [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+    assert parse_data_ascii("") == [17, 31, 73, 47, 23]
+
+
+def test_convert_to_hex():
+    assert convert_to_hex([255]) == "ff"
+    assert convert_to_hex([64, 7, 255]) == "4007ff"
+
+
+def test_knot_multi_empty():
+    assert knot_hash(255, [], rounds=64) == list(n for n in range(256))
+
+
+def test_knot_hash_multi():
+    # ([0],1),2,3,4,5 pos 0, skip 0
+    # 1,0,([2],3),4,5 pos 2, skipt 1
+    # 1),0,3,2,4,([5] pos 5, skip 2
+    # 5,0,3,([2],4),1 pos 4, skip 3
+    # end round 1
+
+    assert knot_hash(5, [2, 2, 2], rounds=1) == [5, 0, 3, 2, 4, 1]  # Round 1
+
+    # 5,0,3,([2],4),1 pos 4, skip 3
+    # 5,0,([3],4),2,1 pos 2, skip 4
+    # 5,0,([4],3),2,1 pos 2, skip 5
+    # 5,0,3,([4],2),1 pos 3, skip 6
+    assert knot_hash(5, [2, 2, 2], rounds=2) == [5, 0, 3, 4, 2, 1]  # Round 2
+
+    # 5,0,3,([4],2),1 pos 3, skip 6
+    # 5),0,3,2,4,([1] pos 5, skip 7
+    # 1,0,([3],2),4,5 pos 2, skip 8
+    # 1,0,2,3,4,5
+    assert knot_hash(5, [2, 2, 2], rounds=3) == [1, 0, 2, 3, 4, 5]  # Round 3
+
+
+def test_solution_2():
+    assert solution_2("") == "a2582a3a0e66e6e86e3812dcb672a272"
+    assert solution_2("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd"
+    assert solution_2("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d"
+    assert solution_2("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e"

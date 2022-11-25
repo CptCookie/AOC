64cd6b9484beaecff06042deb034cde0f0cd2a4e
S. Hartstock <github@hartstock.me>
2022-11-25 10:53:10 +0100

    adjust Python Template

--- Python/Templates/aoc_python_day/{{cookiecutter.Year}}/Day{{cookiecutter.Day}}/test_solution.py
+++ Python/Templates/aoc_python_day/{{cookiecutter.Year}}/Day{{cookiecutter.Day}}/test_solution.py
@@ -3,9 +3,9 @@ from .solution import solution_1, solution_2
 
 def test_solution_1():
     test_input = ""
-    assert False
+    assert solution_1(test_input)
 
 
 def test_solution_2():
     test_input = ""
-    assert False
+    assert solution_2(test_input)

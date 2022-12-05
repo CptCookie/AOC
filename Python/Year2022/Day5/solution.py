872fc262685f05fae2a0637d72de6707667181d4
CptCookie <github@hartstock.me>
2022-12-05 13:11:20 +0100

    black

--- Python/Year2022/Day5/solution.py
+++ Python/Year2022/Day5/solution.py
@@ -7,24 +7,26 @@ def parse_data(puzzle_input: str):
     lines = [l[1::4] for l in lines.splitlines()][:-1:]
 
     stacks = [list(reversed([l[n] for l in lines if l[n] != " "])) for n in range(9)]
-    moves = [[int(v) for v in l] for l in re.findall(r"move (\d+) from (\d+) to (\d+)", moves)]
+    moves = [
+        [int(v) for v in l]
+        for l in re.findall(r"move (\d+) from (\d+) to (\d+)", moves)
+    ]
 
     return stacks, moves
 
 
-def move_crates( stack: list[list[str ] ], amount: int, from_idx: int, to_idx: int ):
-    for _ in range( amount ):
-        stack[ to_idx - 1 ].append( stack[ from_idx - 1 ].pop() )
+def move_crates(stack: list[list[str]], amount: int, from_idx: int, to_idx: int):
+    for _ in range(amount):
+        stack[to_idx - 1].append(stack[from_idx - 1].pop())
     return stack
 
+
 def solution_1(puzzle_input: str):
-    stacks, moves = parse_data( puzzle_input )
+    stacks, moves = parse_data(puzzle_input)
     for m in moves:
         move_crates(stacks, *m)
 
-    return( "".join( [ s.pop() for s in stacks ] ) )
-
-
+    return "".join([s.pop() for s in stacks])
 
 
 def solution_2(puzzle_input: str):

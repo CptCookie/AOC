c9e7a5c4e2fa488f61154a396e7123869def47db
S. Hartstock <github@hartstock.me>
2022-12-06 06:24:06 +0100

    fix formating

--- Python/Year2022/Day1/solution.py
+++ Python/Year2022/Day1/solution.py
@@ -1,15 +1,15 @@
-    def parse_data(puzzle_input: str) -> list[str]:
-        elf_package = []
-        for elf in puzzle_input.split("\n\n"):
-            elf_package.append([int(n) for n in elf.splitlines()])
-        return elf_package
+def parse_data(puzzle_input: str) -> list[str]:
+    elf_package = []
+    for elf in puzzle_input.split("\n\n"):
+        elf_package.append([int(n) for n in elf.splitlines()])
+    return elf_package
 
 
-    def solution_1(puzzle_input: str):
-        elf_packages = parse_data(puzzle_input)
-        return max([sum(e) for e in elf_packages])
+def solution_1(puzzle_input: str):
+    elf_packages = parse_data(puzzle_input)
+    return max([sum(e) for e in elf_packages])
 
 
-    def solution_2(puzzle_input: str):
-        elf_packages = parse_data(puzzle_input)
-        return sum(sorted([sum(e) for e in elf_packages])[-3:])
+def solution_2(puzzle_input: str):
+    elf_packages = parse_data(puzzle_input)
+    return sum(sorted([sum(e) for e in elf_packages])[-3:])

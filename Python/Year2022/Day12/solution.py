3a81bd9e46d5654236a8adc036e2d7dd16a9926c
CptCookie <github@hartstock.me>
2022-12-12 09:43:00 +0100

    2022 Day 12 solution 1, 2 WIP

--- Python/Year2022/Day12/solution.py
+++ Python/Year2022/Day12/solution.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+from time import perf_counter_ns
 
 
 def parse_data(puzzle_input: str) -> list[str]:
@@ -24,10 +25,11 @@ def get_next_states(pos: tuple[int, int], map: list[list[int]]):
     x, y = pos
     new_pos = []
     for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-        if 0 <= y + dy < len(map) and 0 <= x + dx < len(map[y + dy]):
-            if abs(ord(map[y + dy][x + dx]) - ord(map[y][x])) <= 1:
+        try:
+            if ord(map[y + dy][x + dx]) - ord(map[y][x]) <= 1:
                 new_pos.append((x + dx, y + dy))
-
+        except IndexError:
+            pass
     return new_pos
 
 
@@ -36,18 +38,24 @@ def walk(map: list[list[int]], start: tuple[int, int], end: tuple[int, int]):
     next_state.put((1, (start, 0)))
     known_states = dict()
 
+
     while not next_state.empty():
         prio, (state, steps) = next_state.get()
+        if state in known_states and steps >= known_states[state]:
+            continue
+
         known_states[state] = steps
+        potential_next_states = get_next_states(state, map)
 
-        print(state, steps, len(known_states), end="\r")
-
-        for next in get_next_states(state, map):
+        for next in potential_next_states:
             if next not in known_states or known_states[next] > steps + 1:
                 prio = steps + abs(end[0] - next[0]) + abs(end[1] - next[1])
                 next_state.put((prio, (next, steps + 1)))
 
-    return known_states[end]
+    if end in known_states:
+        return known_states[end]
+    else:
+        return 9001
 
 
 def solution_1(puzzle_input: str):
@@ -56,5 +64,6 @@ def solution_1(puzzle_input: str):
 
 
 def solution_2(puzzle_input: str):
-    data = parse_data(puzzle_input)
-    return None
+    map, start, end = parse_data( puzzle_input )
+    all_starts = [(y,x) for y, line in enumerate(map) for x,c in enumerate(line) if c == "a"]
+    return min(walk(map, s, end) for s in all_starts)

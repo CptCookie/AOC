4e4406a4be1bbbbcf6262ce1d6a7f96f9eaab500
S. Hartstock <github@hartstock.me>
2022-12-15 05:50:20 +0100

    update template

--- Python/Templates/aoc_python_day/Year{{cookiecutter.Year}}/Day{{cookiecutter.Day}}/solution.py
+++ Python/Templates/aoc_python_day/Year{{cookiecutter.Year}}/Day{{cookiecutter.Day}}/solution.py
@@ -1,12 +1,12 @@
-def parse_data(puzzle_input: str) -> list[str]:
+def parse_input(puzzle_input: str) -> list[str]:
     return [n for n in puzzle_input.splitlines() if n != ""]
 
 
 def solution_1(puzzle_input: str):
-    data = parse_data(puzzle_input)
+    input = parse_input(puzzle_input)
     return None
 
 
 def solution_2(puzzle_input: str):
-    data = parse_data(puzzle_input)
+    input = parse_input(puzzle_input)
     return None

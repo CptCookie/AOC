5e844526391a79f0deafdab74f6b2e1a355b1d9e
CptCookie <github@hartstock.me>
2022-12-22 08:20:32 +0100

    2022 Day 17 Solution 1

--- Python/Year2022/Day17/solution.py
+++ Python/Year2022/Day17/solution.py
@@ -4,16 +4,12 @@ fall, push, rest
 from utils.list_tools import CircleList
 
 
-def parse_input(puzzle_input: str) -> list[str]:
-    return [n for n in puzzle_input.splitlines() if n != ""]
-
-
 class Tetris:
     pieces = [
         ((2, 0), (3, 0), (4, 0), (5, 0)),  # horizontal piece
         ((2, 1), (3, 1), (4, 1), (3, 2), (3, 0)),  # plus piece
         ((2, 0), (3, 0), (4, 0), (4, 1), (4, 2)),  # inverted L piece
-        ((2, 0), (2, 1), (2, 2), (2, 3), (2, 4)),  # vertical piece
+        ((2, 0), (2, 1), (2, 2), (2, 3)),  # vertical piece
         ((2, 0), (3, 0), (2, 1), (3, 1)),  # block piece
     ]
 
@@ -27,18 +23,22 @@ class Tetris:
 
     def fall(self):
         self.current_piece = [(x, y - 1) for x, y in self.current_piece]
+        # print("fall")
+        # self.print()
 
     def push(self):
+        # print(f"push: {self.jets[self.jet_pnt]}")
         d = -1 if self.jets[self.jet_pnt] == "<" else 1
 
         collision = (
-            (x + d, y) in self.rested_piece or not 0 < x + d <= 6
+            (x + d, y) in self.rested_piece or not 0 <= x + d <= 6
             for x, y in self.current_piece
         )
         if not any(collision):
             self.current_piece = [(x + d, y) for x, y in self.current_piece]
 
         self.jet_pnt += 1
+        # self.print()
 
     def rest(self):
         if any(
@@ -46,8 +46,10 @@ class Tetris:
         ):
             self.rested_piece.extend(self.current_piece)
             self.current_height = max(
-                [y for _, y in self.current_piece] + [self.current_height]
+                [y + 1 for _, y in self.current_piece] + [self.current_height]
             )
+            # print("rest")
+            # self.print()
             return True
         return False
 
@@ -57,19 +59,37 @@ class Tetris:
         ]
         self.next_piece = (self.next_piece + 1) % len(self.pieces)
 
-    def play_stone(self):
+    def play_single_stone(self):
         self.place_next_piece()
         self.push()
         while not self.rest():
             self.fall()
             self.push()
 
+    def play_stones(self, n_stones):
+        for n in range(n_stones):
+            self.play_single_stone()
+
+
+    def print(self):
+        for y in range(self.current_height + 7,-1,-1):
+            for x in range(7):
+                if (x,y) in self.rested_piece:
+                    print("#", end="")
+                elif (x,y) in self.current_piece:
+                    print("@", end="")
+                else:
+                    print(".", end="")
+            print("")
+        print("=======\n")
 
 def solution_1(puzzle_input: str):
-    input = parse_input(puzzle_input)
-    return None
+    field = Tetris([n for n in puzzle_input if n not in [" ", "\n"]])
+    field.play_stones(2022)
+    return field.current_height
 
 
 def solution_2(puzzle_input: str):
-    input = parse_input(puzzle_input)
-    return None
+    field = Tetris([n for n in puzzle_input if n not in [" ", "\n"]])
+    field.play_stones(1000000000000)
+    return field.current_height
--- Python/Year2022/Day17/test_solution.py
+++ Python/Year2022/Day17/test_solution.py
@@ -1,4 +1,4 @@
-from .solution import solution_1, solution_2, parse_input, Tetris
+from .solution import solution_1, solution_2, Tetris
 from utils.tests import assertSublist
 
 TEST_INPUT = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
@@ -6,26 +6,38 @@ TEST_INPUT = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 TEST_DATA = []
 
 
-def test_play():
+def test_play_stone_1():
     field = Tetris([n for n in TEST_INPUT])
-    field.play_stone()
-    assert field.rested_piece == [(2, 0), (3, 0), (4, 0), (5, 0)]
+    field.play_stones(1)
+    assertSublist(field.rested_piece, [(2, 0), (3, 0), (4, 0), (5, 0)])
+    assert field.current_height == 1
 
 
-def test_play_double():
+def test_play_stone_2():
     field = Tetris([n for n in TEST_INPUT])
-    field.play_stone()
-    field.play_stone()
-    assertSublist(field.rested_piece, [(3, 1), (2, 2), (3, 2), (4, 2), (3, 3)])
+    field.play_stones(2)
+    assertSublist(field.rested_piece, [(2, 2), (3, 2),(4,2), (3, 3), (3, 1)])
+    assert field.current_height == 4
 
 
-# def test_parsing():
-#     assert parse_input(TEST_INPUT) == TEST_DATA
+
+def test_play_stone_3():
+    field = Tetris([n for n in TEST_INPUT])
+    field.play_stones(3)
+    assertSublist(field.rested_piece, [(0, 3), (1, 3), (2, 3), (2, 4), (2, 5)])
+    assert field.current_height == 6
 
 
-# def test_solution_1():
-#     assert solution_1(TEST_INPUT)
+def test_play_stone_4():
+    field = Tetris([n for n in TEST_INPUT])
+    field.play_stones(4)
+    assertSublist(field.rested_piece, [(4, 3), (4, 4), (4, 5), (4, 6)])
+    assert field.current_height == 7
 
 
-# def test_solution_2():
-#     assert solution_2(TEST_INPUT)
+def test_solution_1():
+    assert solution_1(TEST_INPUT) == 3068
+
+
+def test_solution_2():
+    assert solution_2(TEST_INPUT) ==

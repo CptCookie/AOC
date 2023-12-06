eed6ba46ef0f25e2f95bb57e29a7cffccde24f93
CptCookie <github@hartstock.me>
2023-12-06 10:06:39 +0100

    2023 Day 4 Python

---
+++ Python/Year2023/Day4/solution.py
@@ -0,0 +1,31 @@
+def parse_input(aoc_input: str) -> list[tuple]:
+    cards = []
+    for line in aoc_input.strip().splitlines():
+        _, card = line.split(": ")
+        draw, win = card.split(" | ")
+        draw_nums = set(int(n) for n in draw.split(" ") if n != "")
+        win_nums = set(int(n) for n in win.split(" ") if n != "")
+        cards.append((draw_nums, win_nums))
+    return cards
+
+
+def solution_1(aoc_input: str):
+    cards = parse_input(aoc_input)
+    score = 0
+
+    for draw, win in cards:
+        matches = len(draw.intersection(win))
+        if matches > 0:
+            score += 2 ** (matches - 1)
+    return score
+
+
+def solution_2(aoc_input: str):
+    cards = parse_input(aoc_input)
+    card_nums = [1 for _ in cards]
+
+    for i, (draw, win) in enumerate(cards):
+        for m in range(len(draw.intersection(win))):
+            card_nums[i + m + 1] += card_nums[i]
+
+    return sum(card_nums)

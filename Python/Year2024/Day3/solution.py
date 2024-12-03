3af69e7ebf260246f6b10a3a3f7010680780f65e
CptCookie <github@hartstock.me>
2024-12-03 10:34:45 +0100

    [2024/03/Python] small optimization

--- Python/Year2024/Day3/solution.py
+++ Python/Year2024/Day3/solution.py
@@ -3,7 +3,7 @@ import re
 
 def solution_1(aoc_input: str) -> int:
     matches = re.finditer(r"mul\((\d{1,3}),(\d{1,3})\)", aoc_input)
-    numbers = [tuple(map(int, m.groups())) for m in matches]
+    numbers = [map(int, m.groups()) for m in matches]
     return sum(a * b for a, b in numbers)
 
 
@@ -11,12 +11,12 @@ def solution_2(aoc_input: str) -> int:
     matches = re.finditer(r"mul\(([\d]{1,3}),([\d]{1,3})\)|don't\(\)|do\(\)", aoc_input)
     count = 0
     do = True
+
     for m in matches:
         if m.group() == "don't()":
             do = False
-        if m.group(0) == "do()":
+        if m.group() == "do()":
             do = True
         elif do:
-            a,b = m.groups()
-            count += int(a)*int(b)
+            count += sum(map(int, m.groups()))
     return count

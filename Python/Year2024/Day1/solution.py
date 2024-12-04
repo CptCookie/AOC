b4f739c01c22869a3cec7e9220d7c6f45151ced4
CptCookie <github@hartstock.me>
2024-12-04 11:56:51 +0100

    [2024/01/Python] optimize solution

--- Python/Year2024/Day1/solution.py
+++ Python/Year2024/Day1/solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def parse_input(aoc_input: str) -> tuple[list[int], list[int]]:
     lines = (map(int, n.split("   ")) for n in aoc_input.splitlines())
     left, right = zip(*lines)
@@ -11,4 +14,7 @@ def solution_1(aoc_input: str):
 
 def solution_2(aoc_input: str):
     left, right = parse_input(aoc_input)
-    return sum(right.count(l) * l for l in left)
+    elem_count = defaultdict(int)
+    for e in right:
+        elem_count[e] += 1
+    return sum(elem_count[l] * l for l in left)

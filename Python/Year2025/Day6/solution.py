86ce600bb2a43abbe74990cd2afc03d34a974cfc
Sebastian Hartstock <git@hartstock.me>
2025-12-06 09:13:10 +0100

    [2025/6/Python] initial solution

---
+++ Python/Year2025/Day6/solution.py
@@ -0,0 +1,12 @@
+def parse_input(aoc_input: str) -> list[str]:
+    return [n for n in aoc_input.splitlines() if n != ""]
+
+
+def solution_1(aoc_input: str):
+    input = parse_input(aoc_input)
+    return None
+
+
+def solution_2(aoc_input: str):
+    input = parse_input(aoc_input)
+    return None

66ae66d4d2831d77b414f3ad430e481d25898f1e
Sebastian Hartstock <git@hartstock.me>
2025-12-06 09:15:01 +0100

    [2025/6/Python] initial solution

--- Python/Year2025/Day6/solution.py
+++ Python/Year2025/Day6/solution.py
@@ -1,12 +1,78 @@
-def parse_input(aoc_input: str) -> list[str]:
-    return [n for n in aoc_input.splitlines() if n != ""]
+from typing import Iterator
+
+Problem = list[str]
+
+
+def parse_part_1(aoc_input: str) -> list[Problem]:
+    problem_lines = []
+
+    for line in aoc_input.splitlines():
+        problem_lines.append([c for c in line.split(" ") if c != ""])
+
+    problems = []
+    for x in range(len(problem_lines[0])):
+        problems.append([problem_lines[y][x] for y in range(len(problem_lines))])
+
+    return problems
+
+
+def parse_part_2(aoc_input: str) -> list[Problem]:
+    lines = [list(line) for line in aoc_input.splitlines()]
+    transformed_problems = []
+    problem = []
+
+    while True:
+        try:
+            col = [line.pop(0) for line in lines]
+            op = col.pop()
+            num = "".join(col).strip()
+            if num == "" and op == " " and problem:
+                transformed_problems.append(problem)
+                problem = []
+                continue
+
+            if op != " ":
+                problem.append(op)
+                # operation marks the begining of a new problem
+
+            problem.append(num)
+        except IndexError:
+            transformed_problems.append(problem)
+            return [
+                list(reversed(problem))
+                for problem in transformed_problems
+                if problem != []
+            ]
+
+
+def is_left(numbers: list[str]):
+    return len(numbers[0]) >= len(numbers[-1])
+
+
+def prod(iter: Iterator[int]):
+    a = next(iter)
+    b = next(iter)
+    prod = a * b
+    for c in iter:
+        prod *= c
+    return prod
+
+
+def solve_problem(problem: Problem) -> int:
+    op = problem[-1]
+    if op == "+":
+        return sum(int(n) for n in problem[:-1])
+    if op == "*":
+        return prod(int(n) for n in problem[:-1])
+
+    raise ValueError(f"Unknown OP: {op}")
 
 
 def solution_1(aoc_input: str):
-    input = parse_input(aoc_input)
-    return None
+    problems = parse_part_1(aoc_input)
+    return sum(solve_problem(p) for p in problems)
 
 
 def solution_2(aoc_input: str):
-    input = parse_input(aoc_input)
-    return None
+    problems = parse_part_2(aoc_input)
+    return sum(solve_problem(p) for p in problems)
--- Python/Year2025/Day6/test_solution.py
+++ Python/Year2025/Day6/test_solution.py
@@ -1,18 +1,35 @@
-from .solution import solution_1, solution_2, parse_input
+from .solution import parse_part_1, parse_part_2, solution_1, solution_2
 
-TEST_INPUT = """
-"""
+TEST_INPUT = """123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  """
 
-TEST_DATA = []
+TEST_DATA_PART_1 = [
+    ["123", "45", "6", "*"],
+    ["328", "64", "98", "+"],
+    ["51", "387", "215", "*"],
+    ["64", "23", "314", "+"],
+]
 
 
-def test_parsing():
-    assert parse_input(TEST_INPUT) == TEST_DATA
+TEST_DATA_PART_2 = [
+    ["356", "24", "1", "*"],
+    ["8", "248", "369", "+"],
+    ["175", "581", "32", "*"],
+    ["4", "431", "623", "+"],
+]
+
+
+def test_parsing_part_1():
+    assert parse_part_1(TEST_INPUT) == TEST_DATA_PART_1
+
+
+def test_parsing_part_2():
+    result = parse_part_2(TEST_INPUT)
+    assert result == TEST_DATA_PART_2
 
 
 def test_solution_1():
-    assert solution_1(TEST_INPUT)
+    assert solution_1(TEST_INPUT) == 4277556
 
 
 def test_solution_2():
-    assert solution_2(TEST_INPUT)
+    assert solution_2(TEST_INPUT) == 3263827
